f817949665ffe85308d44333f33ca359b63005ef
jgomezz <jgomezz@gmail.com>
2024-05-06 23:34:01 -0500

    update sesion 02

---
+++ sesion_02/1_range.py
@@ -0,0 +1,18 @@
+
+# Imprimir los 5 primeros números naturales, empezando en 0
+for nro in range(5): 
+  print(nro)
+
+
+# Imprimir números de 10 a 15
+for nro in range(10,16):
+  print(nro)
+
+
+# Imprimir números de 10 a 15, incrementados de 2 en 2 
+for nro in range(10,16,2):
+  print(nro)
+
+
+# Ejercicio 17 : Imprimir los números de 2 a 10, incrementados en 3, y calcular 
+#                la suma de esos números
