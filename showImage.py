55dd9c89d720f80bc3a19f55fef09b26eac97f3d
jgomezz <jgomezz@gmail.com>
2024-06-10 22:45:31 -0500

    update

--- showImage.py
+++ showImage.py
@@ -1,12 +1,10 @@
 import turtle
 
-
 def get_imagename_not_unid(n):
     return f"assets/overhand-knot-{n}.gif"
 
 def get_imagename_unid(n):
     return f"assets/long-knot-{n}.gif"
-
 
 #turtle.speed(10)
 
@@ -17,11 +15,15 @@ turtle.tracer(0, 0)  # Turn off animation, set delay to 0
 
 # Set up the screen
 screen = turtle.Screen()
-screen.title("Turtle with an Image")
+screen.title("Quipu")
+# Use setup to make the window large enough to fill the screen
+screen.setup(width=0.99, height=0.99)  # Use almost full width and height of the display
 
 # Register the GIF image
 screen.addshape("assets/end-knot.gif")  
 screen.addshape("assets/grid.gif")  
+screen.addshape("assets/top-cord.gif")  
+screen.addshape("assets/top-grid.gif")  
 screen.addshape("assets/primary-cord.gif")  
 screen.addshape("assets/pendant-cord.gif")  
 screen.addshape("assets/long-knot-1.gif")  
@@ -48,15 +50,15 @@ IDX_CEN = 1
 IDX_DEC = 2
 IDX_UNI = 3
 
-numbers =[9999, 1034, 3944, 2546, 1001, 2341, 900, 23]
+numbers =[1000, 2024, 1234 , 5234, 120, 1000, 9999]
 
 x = -350
-y = -300
+y =  80 # 30
 
 image_turtle0 = turtle.Turtle()
 image_turtle0.shape("assets/end-knot.gif")  
 image_turtle0.penup()
-image_turtle0.goto(x-40, 30)  
+image_turtle0.goto(x-40, y)  
 image_turtle0.stamp() 
 
 for number in numbers:
@@ -66,42 +68,43 @@ for number in numbers:
     image_turtle1 = turtle.Turtle()
     image_turtle1.shape("assets/primary-cord.gif")  
     image_turtle1.penup()
-    image_turtle1.goto(x, 30)  
+    image_turtle1.goto(x, y)  
 
     image_turtle2 = turtle.Turtle()
     image_turtle2.shape("assets/grid.gif")  
     image_turtle2.penup()
-    image_turtle2.goto(x, -150)  
+    image_turtle2.goto(x, y-180)  
 
     image_turtle3 = turtle.Turtle()
     image_turtle3.shape("assets/pendant-cord.gif")  
     image_turtle3.penup()
-    image_turtle3.goto(x, -190)  
+    image_turtle3.goto(x, y-220)  
 
     image_turtle4 = turtle.Turtle()
     if str_number[IDX_MIL] != '0':
         image_turtle4.shape(get_imagename_not_unid(str_number[IDX_MIL]))
         image_turtle4.penup()
-        image_turtle4.goto(x, -40)  
+        image_turtle4.goto(x, y-70)  
 
     image_turtle5 = turtle.Turtle()
     if str_number[IDX_CEN] != '0':
         image_turtle5.shape(get_imagename_not_unid(str_number[IDX_CEN]))
         image_turtle5.penup()
-        image_turtle5.goto(x, -150)  
+        image_turtle5.goto(x, y-180)  
 
     image_turtle6 = turtle.Turtle()
     if str_number[IDX_DEC] != '0':
         image_turtle6.shape(get_imagename_not_unid(str_number[IDX_DEC]))
         image_turtle6.penup()
-        image_turtle6.goto(x, -260)  
+        image_turtle6.goto(x, y-290)  
 
     image_turtle7 = turtle.Turtle()
     if str_number[IDX_UNI] != '0':
         image_turtle7.shape(get_imagename_unid(str_number[IDX_UNI]))
         image_turtle7.penup()
-        image_turtle7.goto(x, -370)  
+        image_turtle7.goto(x, y-400)  
 
+    
     x = x + 50
 
     image_turtle1.stamp() 
@@ -111,6 +114,21 @@ for number in numbers:
     image_turtle5.stamp() 
     image_turtle6.stamp() 
     image_turtle7.stamp() 
+
+image_turtle11 = turtle.Turtle()
+image_turtle11.shape("assets/top-cord.gif")  
+image_turtle11.penup()
+image_turtle11.goto(x+200, y+140)  
+
+image_turtle12 = turtle.Turtle()
+image_turtle12.shape("assets/top-grid.gif")  
+image_turtle12.penup()
+image_turtle12.goto(x+185, y+265)  
+
+image_turtle11.stamp() 
+image_turtle12.stamp() 
+
+
 
 turtle.mainloop()
 
